2e0d770500c64a3b5f6f43589ecf906795c66f77
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-12-06 10:07:36 +0530

    Add files via upload

---
+++ t1p10.cpp
@@ -0,0 +1,28 @@
+// Practical 11 lab 06/12/2021
+// Task a : Complete Implementation of Access Inheritance .
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    public :
+       int x , y  ;
+   
+};
+class B : public A
+{
+    public :
+     void display()
+     {
+       x = 10 ; // Inheritance b/c x,y are data members of Class A
+       y = 20 ; // Inheritance b/c x,y are data members of Class A
+       cout<<"X = "<<x<<" Y = "<<y <<endl;
+     }
+};
+int main()
+{
+    B obj ;
+    obj.display(); 
+    return 0;
+}
